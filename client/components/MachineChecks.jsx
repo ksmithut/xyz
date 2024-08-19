128bfe6ce126201ecc9ad2cc498acbc6263a3e30
ksmithut <github@ksmithut.com>
2024-08-19 15:27:39 -0600

    style suspended and stopped machines differently

--- client/components/MachineChecks.jsx
+++ client/components/MachineChecks.jsx
@@ -1,4 +1,6 @@
 import cx from 'clsx'
+import { MoonIcon } from '@heroicons/react/24/solid'
+import { MoonIcon as MoonIconOutline } from '@heroicons/react/24/outline'
 
 /**
  * @param {React.PropsWithChildren<{machine: import('../fly.js').Machine, className?: string}>} props
@@ -11,39 +13,48 @@ export function MachineChecks({ machine, className, children }) {
   const hasCritical = statusGrouping.has('critical')
   const hasWarning = statusGrouping.has('warning')
   const hasPassing = statusGrouping.has('passing')
-  const isStopped = machine.state === 'stopped'
+  const isStopped = machine.state === 'stopped' || machine.state === 'stopping'
+  const isSuspended =
+    machine.state === 'suspended' || machine.state === 'suspending'
 
   return (
     <span
       className={cx(
         'inline-flex items-center gap-x-1 rounded-full px-2 py-1 font-mono text-xs font-bold ring-1 ring-inset',
-        hasCritical
-          ? 'bg-red-100 text-red-900 ring-red-200'
-          : hasWarning
-            ? 'bg-yellow-100 text-yellow-900 ring-yellow-200'
-            : hasPassing
-              ? 'bg-green-100 text-green-900 ring-green-200'
-              : isStopped
-                ? 'bg-gray-300 text-gray-500 ring-gray-400'
+        isStopped || isSuspended
+          ? 'bg-gray-200 text-gray-500 ring-gray-400'
+          : hasCritical
+            ? 'bg-red-100 text-red-900 ring-red-200'
+            : hasWarning
+              ? 'bg-yellow-100 text-yellow-900 ring-yellow-200'
+              : hasPassing
+                ? 'bg-green-100 text-green-900 ring-green-200'
                 : 'text-gray-900 ring-gray-200',
         className
       )}
     >
       {children}
-      {machine.checks?.map((check) => (
-        <svg
-          key={check.name}
-          className={cx('h-2 w-2', {
-            'fill-red-500': check.status === 'critical',
-            'fill-yellow-500': check.status === 'warning',
-            'fill-green-500': check.status === 'passing'
-          })}
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
-      ))}
+      {!(isStopped || isSuspended) &&
+        machine.checks?.map((check) => (
+          <svg
+            key={check.name}
+            className={cx('h-2 w-2', {
+              'fill-red-500': check.status === 'critical',
+              'fill-yellow-500': check.status === 'warning',
+              'fill-green-500': check.status === 'passing'
+            })}
+            viewBox="0 0 6 6"
+            aria-hidden="true"
+          >
+            <circle cx={3} cy={3} r={3} />
+          </svg>
+        ))}
+      {isSuspended && (
+        <MoonIconOutline className="h-4 w-4 text-gray-400" aria-hidden="true" />
+      )}
+      {isStopped && (
+        <MoonIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
+      )}
     </span>
   )
 }
